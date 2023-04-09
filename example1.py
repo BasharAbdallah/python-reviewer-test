bb53342d8ddecd0857dd94a2621c5ebb4f98f345
BasharAbdallah <38089054+BasharAbdallah@users.noreply.github.com>
2023-04-09 15:48:12 -0400

    Update example1.py

--- example1.py
+++ example1.py
@@ -1,25 +1,27 @@
 # exmaple for used in CodeGuru - from course EDX
 
-operation_count = 0
-def main ():
+operations_count = 0
 
-ask_again=true
-while (ask_again):
-a = input(" enter the numerator:")
-b = input(" enter the denomintor:")
-result = perfom_division(a,b)
-print (result)
-ask_again = input("do you want to perform another operation enter yes or no : ")
-if (ask_again=='yes')
-ask_again = true
-else:
-ask_again = false
-print("you performed"+ str(operation_count)+"operation,bye !")
-def perfom_division (a,b)
-global operation_count
-try:
-operation_count+=1
-return int(a)/int(b)
-except Exception as e:
-pass 
-main ()
+def main():
+    ask_again = True
+    while(ask_again):
+        a = input("Enter the numerator: ")
+        b = input("Enter the denominator: ")
+        result = perform_division(a,b)
+        print(result)
+        ask_again = input("Do you want to perform another operation? Enter yes or no: ")
+        if(ask_again == 'yes'):
+            ask_again = True
+        else:
+            ask_again = False
+            print("You performed " + str(operations_count) + " operations, bye!")
+
+def perform_division(a,b):
+    global operations_count
+    try:
+        operations_count += 1
+        return int(a)/int(b)
+    except Exception as e:
+        pass
+
+main()
