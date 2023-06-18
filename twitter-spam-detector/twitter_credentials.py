f34ec07a078079d7a66b3f825d6930bc6ac15398
BasharAbdallah <38089054+BasharAbdallah@users.noreply.github.com>
2023-06-18 16:54:03 -0400

    Add files via upload

---
+++ twitter-spam-detector/twitter_credentials.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  4 13:29:35 2018
+
+@author: bugur
+"""
+#Just holding twitter authentication tokens
+#Twitter uygulaması için gerekli token'ları saklıyoruz
+
+CONSUMER_KEY = ''
+CONSUMER_KEY_SECRET = ''
+ACCES_TOKEN = ''
+ACCES_TOKEN_SECRET = ''
